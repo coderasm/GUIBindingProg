eaaf33eff33c12293481f51e507ff3f5c648414d
pkrueger <philip.krueger@caci.com>
2019-03-27 21:45:16 -0700

    first commit

---
+++ WPFBindingProg/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace WPFBindingProg
+{
+  class ViewModel : INotifyPropertyChanged
+  {
+    private string _text = "";
+    public event PropertyChangedEventHandler PropertyChanged;
+    public string Text {
+      get
+      {
+        return _text;
+      }
+      set
+      {
+        if(_text != value)
+        {
+          _text = value;
+          RaisePropertyChanged("Text");
+        }
+      }
+    }
+
+    public ViewModel()
+    {
+    }
+
+    private void RaisePropertyChanged(string propertyName)
+    {
+      // take a copy to prevent thread issues
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+  }
+}
